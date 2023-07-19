a620cfe84f8efc2a0dd27f668fcd3f47878aa409
harsh <harshkarkar333@gmail.com>
2023-07-19 11:05:02 +0530

    19-7-2023

---
+++ constuctor_program.cpp
@@ -0,0 +1,45 @@
+#include<iostream>
+using namespace std;
+
+class student
+{
+    private:
+    int i;
+    char name[20];
+    float mark[3],total,per;
+
+    public:
+
+    student()
+    {
+        cout<<"enter student name : ";
+        cin>>name;
+        cout<<"enter mark of 3 subject : ";
+        for(i=0; i<3; i++)
+        {
+            cin>>mark[i];
+        }
+        total = mark[0] + mark[1] + mark[2];
+
+        per = (total*100)/300;
+    }
+
+    void display()
+    {
+        cout<<"student name : "<<name<<endl;
+        for ( i = 0; i < 3; i++)
+        {
+            cout<<"subject mark is : "<<mark[i]<<endl;
+        }
+        
+        cout<<"total is : "<<total<<endl;
+        cout<<"per is : "<<per<<endl;
+    }
+
+};
+
+int main()
+{
+    student s;
+    s.display();
+}
